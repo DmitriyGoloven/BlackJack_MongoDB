00a4cc9575c2717ee4a209523b808c6cb5a67ca4
DmitriyHoloven <goloven.dmitriy@gmail.com>
2022-05-09 02:09:00 +0300

    env add

--- server/index.js
+++ server/index.js
@@ -4,14 +4,13 @@ const Koa = require('koa');
 const app = new Koa();
 const  router = require('./router/router')
 const bodyParser = require('koa-bodyparser');
-
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-mongoose.connect('mongodb+srv://DmitriyGoloven:lbvf341120@cluster0.id5tv.mongodb.net/BlackJackTest?retryWrites=true&w=majority', function (err) {
+mongoose.connect(`mongodb+srv://${process.env.USERBD}:${process.env.PASSWORD}@cluster0.id5tv.mongodb.net/BlackJackTest?retryWrites=true&w=majority`, function (err) {
 
     if (err) throw err;
     console.log('Successfully connected');
-
 });
 
  app.use(server(path.join(__dirname, 'public/static')))
